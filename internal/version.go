f84f684056449ef56c28e321fa01224e80ad7528
Adam Shelton <adamshelton@rti.org>
2021-12-08 15:16:31 -0500

    fix: big biscuit

---
+++ internal/version.go
@@ -0,0 +1,3 @@
+package internal
+
+var Version = "v0.1.8"
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
   _ "embed"
+  "fmt"
+  "github.com/KoduIsGreat/wails-go-release-template/internal"
   "github.com/KoduIsGreat/wails-go-release-template/pkg/counter"
   "github.com/wailsapp/wails"
 )
@@ -13,7 +15,7 @@ var js string
 var css string
 
 func main() {
-
+  fmt.Println(internal.Version)
   app := wails.CreateApp(&wails.AppConfig{
     Width:  1024,
     Height: 768,
