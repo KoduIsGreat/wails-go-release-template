39f83551977045ff9f6c55f2df307cca01e7fa0a
Adam Shelton <adamshelton@rti.org>
2021-12-08 09:42:48 -0500

    init commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  _ "embed"
+  "github.com/wailsapp/wails"
+  "github.com/KoduIsGreat/wails-go-release-template/pkg/counter"
+)
+
+//go:embed frontend/build/main.js
+var js string
+
+//go:embed frontend/build/main.css
+var css string
+
+func main() {
+
+  app := wails.CreateApp(&wails.AppConfig{
+    Width:  1024,
+    Height: 768,
+    Title:  "wails-go-release-template",
+    JS:     js,
+    CSS:    css,
+    Colour: "#131313",
+  })
+  app.Bind(&counter.Counter{})
+  app.Run()
+}
